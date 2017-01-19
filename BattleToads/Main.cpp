4465efc83d8144af661d5cf0803cada8d16ff98f
Raul Treviño <rtv031.itc@gmail.com>
2017-01-19 18:45:54 +0100

    MathLib Done

--- BattleToads/Main.cpp
+++ BattleToads/Main.cpp
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include "Application.h"
 #include "Globals.h"
-
 #include "SDL/include/SDL.h"
+#include "MathGeoLib/include/MathGeoLib.h"
+
 #pragma comment( lib, "SDL/libx86/SDL2.lib" )
 #pragma comment( lib, "SDL/libx86/SDL2main.lib" )
+
+
 
 enum main_states
 {
@@ -20,7 +23,17 @@ Application* App = nullptr;
 int main(int argc, char ** argv)
 {
 	ReportMemoryLeaks();
+	float4 vector;
+	vector.x = 0;
+	vector.y = 0;
+	vector.z = 0;
+	vector.w = 0;
+	float4 vector2 = vector.Abs();
+	
 
+
+	
+	
 	int main_return = EXIT_FAILURE;
 	main_states state = MAIN_CREATION;
 
@@ -29,24 +42,24 @@ int main(int argc, char ** argv)
 		switch (state)
 		{
 		case MAIN_CREATION:
-
-			LOG("Application Creation --------------");
+		
+			LOG_GLO("Application Creation --------------");
 			App = new Application();
 			state = MAIN_START;
 			break;
 
 		case MAIN_START:
 
-			LOG("Application Init --------------");
+			LOG_GLO("Application Init --------------");
 			if (App->Init() == false)
 			{
-				LOG("Application Init exits with error -----");
+				LOG_GLO("Application Init exits with error -----");
 				state = MAIN_EXIT;
 			}
 			else
 			{
 				state = MAIN_UPDATE;
-				LOG("Application Update --------------");
+				LOG_GLO("Application Update --------------");
 			}
 
 			break;
@@ -57,7 +70,7 @@ int main(int argc, char ** argv)
 
 			if (update_return == UPDATE_ERROR)
 			{
-				LOG("Application Update exits with error -----");
+				LOG_GLO("Application Update exits with error -----");
 				state = MAIN_EXIT;
 			}
 
@@ -68,10 +81,10 @@ int main(int argc, char ** argv)
 
 		case MAIN_FINISH:
 
-			LOG("Application CleanUp --------------");
+			LOG_GLO("Application CleanUp --------------");
 			if (App->CleanUp() == false)
 			{
-				LOG("Application CleanUp exits with error -----");
+				LOG_GLO("Application CleanUp exits with error -----");
 			}
 			else
 				main_return = EXIT_SUCCESS;
@@ -84,6 +97,6 @@ int main(int argc, char ** argv)
 	}
 
 	RELEASE( App);
-	LOG("Bye :)\n");
+	LOG_GLO("Bye :)\n");
 	return main_return;
 }
