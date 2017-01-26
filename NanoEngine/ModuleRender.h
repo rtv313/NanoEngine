9f06f77bafb0ec0c0509d09c18b0b838fd127462
Raul Treviño <rtv031.itc@gmail.com>
2017-01-26 22:51:32 +0100

    OpenGl installation, first steps

--- NanoEngine/ModuleRender.h
+++ NanoEngine/ModuleRender.h
@@ -3,6 +3,10 @@
 
 #include "Module.h"
 #include "Globals.h"
+#include "Glew/include/GL/glew.h"
+#include "SDL/include/SDL_opengl.h"
+#include <gl/GL.h>
+#include <gl/GLU.h>
 
 struct SDL_Texture;
 struct SDL_Renderer;
--- NanoEngine/ModuleWindow.cpp
+++ NanoEngine/ModuleWindow.cpp
@@ -2,6 +2,15 @@
 #include "Application.h"
 #include "ModuleWindow.h"
 #include "SDL/include/SDL.h"
+#include "Glew/include/GL/glew.h"
+#include "SDL/include/SDL_opengl.h"
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+#define GLEW_STATIC
+#pragma comment( lib, "Glew/lib/Release/Win32/glew32s.lib" )
+
+
 
 ModuleWindow::ModuleWindow()
 {
@@ -16,6 +25,15 @@ ModuleWindow::~ModuleWindow()
 bool ModuleWindow::Init()
 {
 	LOG_GLOBALS("Init SDL window & surface");
+
+	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
 	bool ret = true;
 
 	if(SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -33,9 +51,22 @@ bool ModuleWindow::Init()
 		if(FULLSCREEN == true)
 		{
 			flags |= SDL_WINDOW_FULLSCREEN;
+			
 		}
 
+		flags |= SDL_WINDOW_OPENGL;
+
 		window = SDL_CreateWindow(TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, flags);
+
+		SDL_GL_CreateContext(window);
+
+		GLenum err = glewInit();
+		LOG_GLOBALS("Using Glew %s", glewGetString(GLEW_VERSION));
+		// Should be 2.0
+		LOG_GLOBALS("Vendor: %s", glGetString(GL_VENDOR));
+		LOG_GLOBALS("Renderer: %s", glGetString(GL_RENDERER));
+		LOG_GLOBALS("OpenGL version supported %s", glGetString(GL_VERSION));
+		LOG_GLOBALS("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
 		if(window == nullptr)
 		{
@@ -60,7 +91,8 @@ bool ModuleWindow::CleanUp()
 	//Destroy window
 	if(window != nullptr)
 	{
-		SDL_DestroyWindow(window);
+		SDL_GL_DeleteContext(window);
+		//SDL_DestroyWindow(window);
 	}
 
 	//Quit SDL subsystems
