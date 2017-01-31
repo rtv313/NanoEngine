d2f58b60d19fb8700644f9940e066f3c367b8b1d
Drixide77 <adria.abella.93@gmail.com>
2017-01-31 21:05:04 +0100

    Weird merge error solved

--- NanoEngine/ModuleRender.cpp
+++ NanoEngine/ModuleRender.cpp
@@ -127,7 +127,7 @@ update_status ModuleRender::PostUpdate()
 	  -0.5f, 0.5f, -0.5f,//G
 	  0.5f, 0.5f, -0.5f, // H
   };
-<<<<<<< HEAD
+
   uint indices[] = {
     0,1,2,
     1,3,2,
@@ -141,7 +141,7 @@ update_status ModuleRender::PostUpdate()
     7,6,3,
     0,4,1,
     4,5,1};
-=======
+
   /*GLfloat cubecolors[] = {
 	  0.5f, 0.5f, 0.5f, // A
 	  0.0f, 0.0f, 255.0f, // B
@@ -152,8 +152,7 @@ update_status ModuleRender::PostUpdate()
 	  255.0f, 255.0f, 0.0f,//G
 	  255.0f, 255.0f, 255.0f, // H
   };*/
-  uint indices[] = {0,1,2,1,3,2,5,7,3,3,1,5,4,6,7,4,7,5,2,6,4,4,0,2,6,2,3,7,6,3,0,4,1,4,5,1};
->>>>>>> origin/master
+
   glGenBuffers(1, (GLuint*) &(my_id));
   glBindBuffer(GL_ARRAY_BUFFER, my_id);
   glBufferData(GL_ARRAY_BUFFER, sizeof(float)*36* 3, vertices, GL_STATIC_DRAW);
